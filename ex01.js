6051221bd41fe8cd11559c730a157f4cc9e8e7c8
zoonmee <zoonmee>
2023-07-11 12:28:08 +0700

    i

---
+++ ex01.js
@@ -0,0 +1,16 @@
+var t = setInterval(function() {
+    console.log('test2')
+  }, 1000)
+  console.log('test1')
+
+  const express = require('express')
+  const app = express()
+  const port = 3000
+  
+  app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+  })
